4f0cfb1b70280d72988ac0fdfe777ed402b3f99b
caoliang <sg_caoliang@yahoo.com.sg>
2020-11-14 13:54:29 +0800

    Update batch script for testing;

--- visualization/bin/start_all.bat
+++ visualization/bin/start_all.bat
@@ -3,7 +3,7 @@ call D:\App\Anaconda3\Scripts\activate.bat D:\App\Anaconda3
 call conda activate irs
 set path=D:\App\Anaconda3\envs\irs;%path%
 D:
-cd "D:\ISS\MTech2020\ISY5003\Practice Module\cloudpoint\bin"
+cd "D:\ISS\MTech2020\ISY5003\Practice Module\3DMapping\visualization\bin"
 
 start start_backend_services.bat
 
